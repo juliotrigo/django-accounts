85ab4b23ec39cb88319cf13102ff4895f9ae27fc
juliotrigo <juliotrigocom@gmail.com>
2013-05-03 23:58:57 +0100

    Adding package accounts/forms.

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,35 +2,11 @@
 
 """accounts admin."""
 
-from django import forms
 from django.contrib import admin
 from django.contrib import auth
 
 from accounts.models import ExtendedUser
-
-class ExtendedUserCreationForm(auth.forms.UserCreationForm):
-    """A form for creating users."""
-    
-    #https://groups.google.com/forum/?fromgroups=#!topic/django-users/kOVEy9zYn5c
-    def clean_username(self):
-        # Since User.username is unique, this check is redundant,
-        # but it sets a nicer error message than the ORM. See #13147.
-        username = self.cleaned_data["username"]
-        try:
-            ExtendedUser._default_manager.get(username=username)
-        except ExtendedUser.DoesNotExist:
-            return username
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
-
-    
-    class Meta(auth.forms.UserCreationForm.Meta):
-        model = ExtendedUser
-
-class ExtendedUserChangeForm(auth.forms.UserChangeForm):
-    """A form for updating users."""
-    
-    class Meta(auth.forms.UserChangeForm.Meta):
-        model = ExtendedUser
+from accounts.forms import ExtendedUserCreationForm, ExtendedUserChangeForm
 
 class ExtendedUserAdmin(auth.admin.UserAdmin):
     """ExtendedUser Admin."""
