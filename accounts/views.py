3212c014c2aa8da0976463f26dd57a9835fa1e1c
juliotrigo <juliotrigocom@gmail.com>
2013-05-04 23:18:39 +0100

    Using RequestContext.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 @login_required
 def profile(request):
-    context = {'user': request.user}
-    return render_to_response('accounts/profile.xhtml', context)
+    return render_to_response('accounts/profile.xhtml', 
+                              context_instance=RequestContext(request))
