f636c60506d57997e2e5d2d7436f8c756494216a
juliotrigo <juliotrigocom@gmail.com>
2014-02-15 22:18:48 +0000

    distutils / setuptools. Distributing: setup, MANIFEST.in, version, requirements.

--- accounts/__init__.py
+++ accounts/__init__.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
-"""accounts - A Django app to manage user accounts."""
+"""accounts
 
-__version__ = "0.1.3"
+    A Django app to manage user accounts.
+
+    :copyright: (c) 2013–2014 by Julio Vicente Trigo Guijarro.
+    :license: BSD, see LICENSE for more details.
+
+"""
+
+__version__ = '0.2.0'
---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""django-accounts setup."""
+
+from __future__ import print_function, unicode_literals
+
+from setuptools import setup, find_packages
+
+import accounts
+
+setup(
+    name='accounts',
+    version=accounts.__version__,
+    author='Julio Vicente Trigo Guijarro',
+    author_email='',
+    url='http://github.com/juliotrigo/django-accounts/',
+    description='A Django app to manage user accounts.',
+    long_description=accounts.__doc__,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Natural Language :: Spanish',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    license='BSD',
+    platforms='any',
+    packages = find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
